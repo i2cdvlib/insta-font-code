256e08301de261323875743be8bac4cd6bb79a60
i2cdvlib <91401315+i2cdvlib@users.noreply.github.com>
2023-04-04 22:27:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import streamlit as st
 
-st.image('https://i0.wp.com/www.dafontfree.io/wp-content/uploads/2020/12/instagram-new.png?resize=1100%2C750&ssl=1', width=300)
+# Add the Instagram font image
+st.image('https://i0.wp.com/www.dafontfree.io/wp-content/uploads/2020/12/instagram-new.png?resize=1100%2C750&ssl=1', 
+         width=300, 
+         output_format='PNG', 
+         use_column_width=True)
