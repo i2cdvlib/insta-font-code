a7f2fc489cd5e62a0074a2ab2190440521af995d
i2cdvlib <91401315+i2cdvlib@users.noreply.github.com>
2023-04-05 19:21:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
 import streamlit as st
 
+# Add CSS to center the Instagram font image and style the text boxes
+st.write(
+    """
+    <style>
+    .center {
+        display: flex;
+        justify-content: center;
+    }
+    .small-font {
+        font-size: 12px;
+    }
+    .small-img {
+        width: 200px;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
 # Add the Instagram font image
 st.image('https://i0.wp.com/www.dafontfree.io/wp-content/uploads/2020/12/instagram-new.png?resize=1100%2C750&ssl=1', 
-         width=300, 
+         width=200, 
          output_format='PNG', 
-         use_column_width=True)
+         use_column_width=True,
+         class_='center small-img')
 
 # Add text boxes for the username and password fields
-username = st.text_input("Username")
-password = st.text_input("Password", type="password")
+username = st.text_input("Username", value="", max_chars=None, key=None, type='default', 
+                         help=None, placeholder='Username', 
+                         autocomplete='on', 
+                         class_='small-font')
+password = st.text_input("Password", value="", max_chars=None, key=None, type='password', 
+                         help=None, placeholder='Password', 
+                         autocomplete='on', 
+                         class_='small-font')
