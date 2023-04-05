7019227841530d1a4652f74909a281986848d536
i2cdvlib <91401315+i2cdvlib@users.noreply.github.com>
2023-04-05 19:07:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ st.image('https://i0.wp.com/www.dafontfree.io/wp-content/uploads/2020/12/instagr
          width=300, 
          output_format='PNG', 
          use_column_width=True)
+
+# Add text boxes for the username and password fields
+username = st.text_input("Username")
+password = st.text_input("Password", type="password")
