a6d1c853ce5d45a48f9bbcc21e470afb0092f1b3
i2cdvlib <91401315+i2cdvlib@users.noreply.github.com>
2023-04-06 11:05:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import streamlit as st
 
-# Define the HTML code for the text box
-html_code = '''
-<input aria-label="Phone number, username, or email" aria-required="true" autocapitalize="off" autocorrect="off" maxlength="75" name="username" type="text" class="_aa4b _add6 _ac4d" value="">
-'''
+# Define custom CSS style for the text input element
+custom_css = """
+<style>
+.stInput > input {
+    padding-left: 25px;
+    background-image: url('https://img.icons8.com/material-sharp/24/000000/username.png');
+    background-repeat: no-repeat;
+    background-position: 5px center;
+}
+</style>
+"""
 
-# Display the HTML code in Streamlit
-st.write(html_code, unsafe_allow_html=True)
+# Render the custom CSS style using the Streamlit's write() function
+st.write(custom_css, unsafe_allow_html=True)
+
+# Create a text input element with a custom class name
+username = st.text_input(" ", value="", max_chars=75, key='username', class_='stInput')
+
+# Display the entered value
+st.write("Entered value:", username)
