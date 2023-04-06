cc6604f46c664479578f35764aa7fbc36c9e4c2a
i2cdvlib <91401315+i2cdvlib@users.noreply.github.com>
2023-04-06 12:21:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import streamlit as st
 
-# Render the label using Markdown with HTML
-st.markdown('<label for="username">Enter username:</label>', unsafe_allow_html=True)
-
-# Create a text input element
-username = st.text_input("", value="", max_chars=75, key='username')
+# Create a text input element with a default placeholder value
+username = st.text_input("Enter your username", value="", max_chars=75, key='username')
 
 # Display the entered value
 st.write("Entered value:", username)
