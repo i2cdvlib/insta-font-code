2cb36da2d1bc75d4033fcb235afd081305e86d3c
i2cdvlib <91401315+i2cdvlib@users.noreply.github.com>
2023-04-06 10:58:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,9 @@
 import streamlit as st
 
-# Define the HTML code
+# Define the HTML code for the text box
 html_code = '''
-<div style="background-color: #F8D7DA; padding: 20px; border-radius: 10px;">
-    <h1 style="color: #721C24; text-align: center;">Welcome to my Streamlit app!</h1>
-    <p style="color: #721C24; text-align: center;">Here's some text that I've styled using HTML and CSS.</p>
-</div>
+<input aria-label="Phone number, username, or email" aria-required="true" autocapitalize="off" autocorrect="off" maxlength="75" name="username" type="text" class="_aa4b _add6 _ac4d" value="">
 '''
 
-# Define the CSS code
-css_code = '''
-body {
-    font-family: Arial, sans-serif;
-    background-color: #F0F2F6;
-    padding: 20px;
-}
-
-h1 {
-    font-size: 36px;
-    text-align: center;
-}
-
-p {
-    font-size: 24px;
-    text-align: center;
-}
-'''
-
-# Render the HTML and CSS in Streamlit
-st.markdown(html_code, unsafe_allow_html=True)
-st.write('')
-st.markdown(f'<style>{css_code}</style>', unsafe_allow_html=True)
-
-# Add some Streamlit elements below the HTML and CSS
-st.write('Here are some Streamlit elements:')
-st.slider('Select a value', 0, 10, 5)
-st.button('Click me')
+# Display the HTML code in Streamlit
+st.write(html_code, unsafe_allow_html=True)
