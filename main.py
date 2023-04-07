2e94cc84f8802c12045e36652c8d08566be71ebc
i2cdvlib <91401315+i2cdvlib@users.noreply.github.com>
2023-04-07 14:17:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,15 +9,14 @@ image_style = f"background-image: url({image_url}); background-size: contain; ba
 st.markdown(f'<div style="{image_style}"></div>', unsafe_allow_html=True)
 
 # Add text input boxes for phone number, email, and Instagram ID
-info = st.text_input("Phone Number, Email, Instagram ID")
+info = st.text_input("Phone Number, Email, Instagram ID", max_chars=50, key="info", height=30)
 
 # Add password input field
-password = st.text_input("Password", type="password")
+password = st.text_input("Password", type="password", height=30)
 
 # Add login button
 login_button_style = "background-color: blue; color: white; padding: 10px; text-align: center; font-size: 18px; border-radius: 5px; width: 150px; margin: 0 auto;"
-login_button_text = '<span style="display:inline-block;vertical-align:middle">Log In</span>'
-login_button = st.button(login_button_text, key="login_button", help="Click to login", unsafe_allow_html=True, style=login_button_style)
+login_button = st.button("Log In", key="login_button", help="Click to login", style=login_button_style)
 
 # Display a message when login button is clicked
 if login_button:
