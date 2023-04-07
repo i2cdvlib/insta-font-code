10d39a87e0df6a72dc46a593c27e05f179f4a7cd
i2cdvlib <91401315+i2cdvlib@users.noreply.github.com>
2023-04-07 13:53:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,21 +3,21 @@ import streamlit as st
 # Set page title
 st.set_page_config(page_title="Login Page", page_icon=None, layout="wide")
 
-# Load and display image
+# Centered image
 image_url = "https://www.dafont.com/forum/attach/orig/8/1/815933.png?1"
-image_style = f"background-image: url({image_url}); background-size: contain; background-repeat: no-repeat; height: 150px; width: 150px;"
+image_style = f"background-image: url({image_url}); background-size: contain; background-repeat: no-repeat; height: 150px; width: 150px; margin: 0 auto;"
 st.markdown(f'<div style="{image_style}"></div>', unsafe_allow_html=True)
 
-# Add text area input for phone number, email, and Instagram ID
-info = st.text_area("Phone Number, Email, Instagram ID", height=100)
+# Add text input boxes for phone number, email, and Instagram ID
+info = st.text_input("Phone Number, Email, Instagram ID")
 
 # Add password input field
 password = st.text_input("Password", type="password")
 
 # Add login button
-login_button_style = "background-color: blue; color: white; padding: 10px; text-align: center; font-size: 18px; border-radius: 5px;"
+login_button_style = "background-color: blue; color: white; padding: 10px; text-align: center; font-size: 18px; border-radius: 5px; width: 150px; margin: 0 auto;"
 login_button_text = '<span style="display:inline-block;vertical-align:middle">Log In</span>'
-login_button = st.button(login_button_text, key="login_button", help="Click to login", style=login_button_style)
+login_button = st.button(login_button_text, key="login_button", help="Click to login", unsafe_allow_html=True, style=login_button_style)
 
 # Display a message when login button is clicked
 if login_button:
